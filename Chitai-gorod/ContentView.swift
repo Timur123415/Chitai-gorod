e381bdebc505d43e170e79f9042b1bffc15fb88f
Timur Belov <timur.belov.16@bk.ru>
2025-02-19 09:41:09 +0300

    Второй коммит

--- Chitai-gorod/ContentView.swift
+++ Chitai-gorod/ContentView.swift
@@ -4,24 +4,20 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject var countCart = CountCart()
-    @State var selectedTag = 0
     var body: some View {
             ZStack {
-                TabView(selection: $selectedTag) {
+                TabView {
                     MainView()
                         .tabItem {
                             Label("Главная", systemImage: "house")
-                                .tag(0)
                         }.toolbarBackground(.visible, for: .tabBar)
                         .toolbarBackground(Color.gray.opacity(0.1), for: .tabBar)
                     CatalogView()
-                        .tag(1)
                         .tabItem {
                             Label("Каталог", systemImage: "books.vertical")
                         }.toolbarBackground(.visible, for: .tabBar)
                         .toolbarBackground(Color.gray.opacity(0.1), for: .tabBar)
                     CartView()
-                        .tag(3)
                         .tabItem {
                             Label("Корзина", systemImage: "cart")
                             
@@ -30,14 +26,12 @@ struct ContentView: View {
                     
                 
                     BookmarkView()
-                        .tag(4)
                         .padding()
                         .tabItem {
                             Label("Закладки", systemImage: "bookmark")
                         }.toolbarBackground(.visible, for: .tabBar)
                         .toolbarBackground(Color.gray.opacity(0.1), for: .tabBar)
                     ProfileView()
-                        .tag(5)
                         .tabItem {
                             Label("Профиль", systemImage: "person")
                         }.toolbarBackground(.visible, for: .tabBar)
